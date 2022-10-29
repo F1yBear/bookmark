c6963bb87f25ee5252eb831eedb6f8030410d851
f1ybear <wangfeixiong126@gmail.com>
2022-10-30 00:17:27 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/bookmark/' : '/',
-  base: '/bookmark/',
+  base: process.env.NODE_ENV === 'production' ? '/bookmark/' : '/',
   build: {
     outDir: "./docs"
   },
