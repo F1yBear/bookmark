16c6ae8f2b69b0a3b40c5314ff09864f57e01d91
f1ybear <wangfeixiong126@gmail.com>
2022-10-30 01:04:11 +0800

    deploy

--- src/http/request.js
+++ src/http/request.js
@@ -3,7 +3,16 @@ import axios from 'axios'
 axios.defaults.timeout = 50000
 //跨域凭证
 axios.defaults.withCredentials = false
-axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
+switch (process.env.NODE_ENV) {
+    case 'development':{
+        axios.defaults.baseURL = "/"
+        break
+    }
+    case 'production':{
+        axios.defaults.baseURL = "https://f1ybear.github.io/bookmark/"
+        break
+    }
+}
 
 //响应和请求 拦截器配置
 
